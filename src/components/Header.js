30b60a8de7a78ad64759bc10853b8133ebc32c6b
Dakota Bryant <bryant.dakota@gmail.com>
2017-05-15 13:50:09 -0500

    Added first component. Styles suck, but w/e

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './header.css';
+
+const Header = (props) => {
+  return (
+    <header>
+      <a href="">What?</a>
+      <a href="">New Game +</a>
+    </header>
+  )
+}
+
+export default Header;
